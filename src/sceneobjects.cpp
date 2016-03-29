16888228ca86b94f028373384450c5425d9804e4
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-29 17:09:00 -0400

    Pole mesh definition progress

--- src/sceneobjects.cpp
+++ src/sceneobjects.cpp
@@ -23,6 +23,24 @@ CFloor::CFloor() {
 }
 
 CPole::CPole() {
+    //** TODO: Define UVs
+    // Bottom hex
+    this->vertices.push_back(new CVertex(0, *new CPoint3D(-3, 0, 0), 0, 0));
+    this->vertices.push_back(new CVertex(1, *new CPoint3D(-0.5, 0, 0.866), 0, 0));
+    this->vertices.push_back(new CVertex(2, *new CPoint3D(0.5, 0, 0.866), 0, 0));
+    this->vertices.push_back(new CVertex(3, *new CPoint3D(3, 0, 0), 0, 0));
+    this->vertices.push_back(new CVertex(4, *new CPoint3D(0.5, 0, -0.866), 0, 0));
+    this->vertices.push_back(new CVertex(5, *new CPoint3D(-0.5, 0, -0.866), 0, 0));
+    
+    // Top hex
+    this->vertices.push_back(new CVertex(6, *new CPoint3D(-3, 6, 0), 0, 0));
+    this->vertices.push_back(new CVertex(7, *new CPoint3D(-0.5, 6, -0.866), 0, 0));
+    this->vertices.push_back(new CVertex(8, *new CPoint3D(0.5, 6, -0.866), 0, 0));
+    this->vertices.push_back(new CVertex(9, *new CPoint3D(3, 6, 0), 0, 0));
+    this->vertices.push_back(new CVertex(10, *new CPoint3D(0.5, 6, 0.866), 0, 0));
+    this->vertices.push_back(new CVertex(11, *new CPoint3D(-0.5, 6, 0.866), 0, 0));
+    
+    //** TODO: Tris
     
 }
 
