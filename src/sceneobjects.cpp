b777a92c1f126a78c2396c58cf131cf177467438
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-29 15:44:11 -0400

    Added scene components stubs

---
+++ src/sceneobjects.cpp
@@ -0,0 +1,27 @@
+//
+//  sceneobjects.cpp
+//  8trd147-d2
+//
+//  Created by Frédéric Bolduc on 2016-03-29.
+//
+//
+
+#include <stdio.h>
+
+#include "sceneobjects.h"
+
+CFloor::CFloor() {
+
+}
+
+CPole::CPole() {
+    
+}
+
+CSheet::CSheet() {
+    
+}
+
+CRope::CRope() {
+    
+}
---
+++ src/sceneobjects.h
@@ -0,0 +1,34 @@
+//
+//  sceneobjects.h
+//  8trd147-d2
+//
+//  Created by Frédéric Bolduc on 2016-03-29.
+//
+//
+
+#ifndef sceneobjects_h
+#define sceneobjects_h
+
+#include "mesh.h"
+
+class CFloor : public CMesh {
+public:
+    CFloor();
+};
+
+class CPole : public CMesh {
+public:
+    CPole();
+};
+
+class CSheet : public CMesh {
+public:
+    CSheet();
+};
+
+class CRope : public CMesh {
+public:
+    CRope();
+};
+
+#endif /* sceneobjects_h */
