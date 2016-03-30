1e78b4a5215f4f1f837fd62fc1f63caa39146847
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-30 13:50:59 -0400

    Correct normals for pole

--- src/sceneobjects.cpp
+++ src/sceneobjects.cpp
@@ -41,35 +41,35 @@ CPole::CPole() {
     this->vertices.push_back(new CVertex(11, *new CPoint3D(-0.05, 6,  0.08), 0, 0));
     
     // Bottom hex tris
-    this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[1], this->vertices[5]));
-    this->triangles.push_back(new CTriangle(this->vertices[1], this->vertices[2], this->vertices[5]));
-    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[4], this->vertices[5]));
-    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[3], this->vertices[4]));
+    this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[5], this->vertices[1]));
+    this->triangles.push_back(new CTriangle(this->vertices[1], this->vertices[5], this->vertices[2]));
+    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[5], this->vertices[4]));
+    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[4], this->vertices[3]));
     
     // Top hex tris
-    this->triangles.push_back(new CTriangle(this->vertices[6], this->vertices[7], this->vertices[11]));
-    this->triangles.push_back(new CTriangle(this->vertices[7], this->vertices[10], this->vertices[11]));
-    this->triangles.push_back(new CTriangle(this->vertices[7], this->vertices[8], this->vertices[10]));
-    this->triangles.push_back(new CTriangle(this->vertices[8], this->vertices[9], this->vertices[10]));
+    this->triangles.push_back(new CTriangle(this->vertices[6], this->vertices[11], this->vertices[7]));
+    this->triangles.push_back(new CTriangle(this->vertices[7], this->vertices[11], this->vertices[10]));
+    this->triangles.push_back(new CTriangle(this->vertices[7], this->vertices[10], this->vertices[8]));
+    this->triangles.push_back(new CTriangle(this->vertices[8], this->vertices[10], this->vertices[9]));
     
     // Lateral faces tris
-    this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[5], this->vertices[7]));
-    this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[7], this->vertices[6]));
+    this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[7], this->vertices[5]));
+    this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[6], this->vertices[7]));
 
-    this->triangles.push_back(new CTriangle(this->vertices[4], this->vertices[7], this->vertices[5]));
-    this->triangles.push_back(new CTriangle(this->vertices[4], this->vertices[8], this->vertices[7]));
+    this->triangles.push_back(new CTriangle(this->vertices[4], this->vertices[5], this->vertices[7]));
+    this->triangles.push_back(new CTriangle(this->vertices[4], this->vertices[7], this->vertices[8]));
 
-    this->triangles.push_back(new CTriangle(this->vertices[4], this->vertices[9], this->vertices[8]));
-    this->triangles.push_back(new CTriangle(this->vertices[4], this->vertices[3], this->vertices[9]));
+    this->triangles.push_back(new CTriangle(this->vertices[4], this->vertices[8], this->vertices[9]));
+    this->triangles.push_back(new CTriangle(this->vertices[4], this->vertices[9], this->vertices[3]));
 
-    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[9], this->vertices[3]));
-    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[10], this->vertices[9]));
+    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[3], this->vertices[9]));
+    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[9], this->vertices[10]));
 
-    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[11], this->vertices[10]));
-    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[1], this->vertices[11]));
+    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[10], this->vertices[11]));
+    this->triangles.push_back(new CTriangle(this->vertices[2], this->vertices[11], this->vertices[1]));
 
-    this->triangles.push_back(new CTriangle(this->vertices[1], this->vertices[0], this->vertices[11]));
-    this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[6], this->vertices[11]));
+    this->triangles.push_back(new CTriangle(this->vertices[1], this->vertices[11], this->vertices[0]));
+    this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[11], this->vertices[6]));
 }
 
 CSheet::CSheet() {
