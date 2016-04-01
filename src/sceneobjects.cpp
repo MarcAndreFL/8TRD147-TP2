6fa60554125aade24ff81162c44328e64847f40a
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-31 22:02:06 -0400

    Restored floor vertices

--- src/sceneobjects.cpp
+++ src/sceneobjects.cpp
@@ -14,10 +14,10 @@
 #include "glutil.h"
 
 CFloor::CFloor() {
-    this->vertices.push_back(new CVertex(0, *new CPoint3D( 4, 0, 4), 1, 1));
-    this->vertices.push_back(new CVertex(1, *new CPoint3D( 4, 0.3, -4), 1, 0));
-    this->vertices.push_back(new CVertex(2, *new CPoint3D(-4, 0.3, -4), 0, 0));
-    this->vertices.push_back(new CVertex(3, *new CPoint3D(-4, 0, 4), 0, 1));
+    this->vertices.push_back(new CVertex(0, *new CPoint3D( 4, 0,  4), 1, 1));
+    this->vertices.push_back(new CVertex(1, *new CPoint3D( 4, 0, -4), 1, 0));
+    this->vertices.push_back(new CVertex(2, *new CPoint3D(-4, 0, -4), 0, 0));
+    this->vertices.push_back(new CVertex(3, *new CPoint3D(-4, 0,  4), 0, 1));
     
     this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[1], this->vertices[2]));
     this->triangles.push_back(new CTriangle(this->vertices[0], this->vertices[2], this->vertices[3]));
